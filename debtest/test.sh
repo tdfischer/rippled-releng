84dba685d6965eb2cf8b16a209c7d04f826ea2f1
Torrie Fischer <torrie@ripple.com>
2015-04-30 17:11:13 -0700

    Add ability to test a specific rippled version

--- debtest/test.sh
+++ debtest/test.sh
@@ -1,6 +1,12 @@
+set -e
+sudo add-apt-repository -y ppa:afrank/boost
 sudo add-apt-repository -y "deb [arch=amd64] http://mirrors.ripple.com/ubuntu trusty contrib stable $APT_COMPONENT"
-sudo apt-get update
-sudo apt-get -y install rippled
+sudo apt-get update -qq
+if [ -n "$RIPPLED_VERSION" ]; then
+  sudo apt-get -y install rippled=$RIPPLED_VERSION
+else
+  sudo apt-get -y install rippled
+fi
 rippled --net --conf /etc/rippled/rippled.cfg &
 sleep 30
-kill %
+pkill rippled
