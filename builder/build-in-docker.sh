666634f134fffc13974f414c71c2bb0e96637bbd
Torrie Fischer <torrie@ripple.com>
2015-05-07 15:15:14 -0700

    Add some command args to build-in-docker.sh

--- builder/build-in-docker.sh
+++ builder/build-in-docker.sh
@@ -1,7 +1,34 @@
 #!/usr/bin/env sh
 
-if [ -n "$1" ];then
-  RIPPLED_SRC_ARG="-v $(realpath $1):/root/src/rippled"
+SRC=""
+REVISION="develop"
+
+while [[ $# > 0 ]]
+do
+  key="$1"
+  case $key in
+    -s|--src)
+      SRC="$2"
+      shift
+      ;;
+    -r|--revision)
+      REVISION="$2"
+      shift
+      ;;
+    --help)
+      echo "Usage: $0 --src git://foo --revision treeish"
+      exit 0
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ -n "$SRC" ];then
+  RIPPLED_SRC_ARG="-v $(realpath $SRC):/root/src/rippled"
 fi
 
 GIT_EMAIL=$(git config --get user.email)
@@ -10,6 +37,8 @@ GIT_NAME=$(git config --get user.name)
 docker run -t -i \
   -e GIT_EMAIL="$GIT_EMAIL" \
   -e GIT_NAME="$GIT_NAME" \
+  -e GIT_UPSTREAM="$REVISION" \
+  -v `pwd`:/root/src/rippled-releng \
   -v ~/.gnupg:/root/.gnupg \
   $RIPPLED_SRC_ARG \
     tdfischer/rippled-deb-packager
